d9ffd03d954653e1f85e9936fe42383139b5bdac
Vladislav Yurin <vdiurin@spasibosb.ru>
2025-09-08 14:30:37 +0300

    fix tests

--- api-gateway/src/test/java/ru/mentor/controller/AuthControllerTest.java
+++ api-gateway/src/test/java/ru/mentor/controller/AuthControllerTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,18 +14,19 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import ru.mentor.ApiGatewayApplication;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import ru.mentor.utils.AuthTestUtil;
 
 @Testcontainers
 @SpringBootTest(classes = ApiGatewayApplication.class)
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class AuthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private AuthTestUtil testUtil;
 
     @Container
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
@@ -54,50 +55,13 @@ public class AuthControllerTest {
 
     @Test
     void reg_Success() throws Exception {
-        mockMvc.perform(post("/auth/reg")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content("""
-                                                 {
-                                                     "username": "test@example.com",
-                                                     "password": "testtesttest",
-                                                     "tgName": "@tgRandomName",
-                                                     "firstName": "randomName",
-                                                     "lastName": "randomLastName"
-                                                 }
-                                                 """))
-               .andExpect(status().isOk())
-               .andExpect(jsonPath("$.accessToken").exists())
-               .andExpect(jsonPath("$.refreshToken").exists());
+        testUtil.getRegistration(mockMvc);
     }
 
     @Test
     void login_Success() throws Exception {
-        // Регистрация пользователя
-        mockMvc.perform(post("/auth/reg")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content("""
-                                                 {
-                                                     "username": "test1@example.com",
-                                                     "password": "testtesttest",
-                                                     "tgName": "@tgRandomName",
-                                                     "firstName": "randomName",
-                                                     "lastName": "randomLastName"
-                                                 }
-                                                 """))
-               .andExpect(status().isOk());
-
-        // Авторизация
-        mockMvc.perform(post("/auth/login")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content("""
-                                                 {
-                                                     "username": "test1@example.com",
-                                                     "password": "testtesttest"
-                                                 }
-                                                 """))
-               .andExpect(status().isOk())
-               .andExpect(jsonPath("$.accessToken").exists())
-               .andExpect(jsonPath("$.refreshToken").exists());
+        testUtil.getRegistration(mockMvc);
+        testUtil.getAuth(mockMvc);
     }
 
 }
