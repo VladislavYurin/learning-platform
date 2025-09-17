9a7cec675aadc62d71aff9534d34f3587cd0e3d9
Leroy <lskorpl@mail.ru>
2025-09-17 07:36:21 +0300

    LELP 21: Исправил звездочку

--- common-lib/src/main/java/ru/mentor/entity/NotificationEntity.java
+++ common-lib/src/main/java/ru/mentor/entity/NotificationEntity.java
@@ -1,6 +1,16 @@
 package ru.mentor.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
