f2761ee9130692f31d673a1bb02d768906b02a56
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2025-10-22 17:04:23 +0600

    LEPL-23: промежуточные изменения по таске.

--- api-gateway/src/main/java/ru/mentor/controller/admin/AdminCourseController.java
+++ api-gateway/src/main/java/ru/mentor/controller/admin/AdminCourseController.java
@@ -14,69 +14,91 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.mentor.dto.CourseDto;
+//import ru.mentor.dto.CourseDto;
+import ru.mentor.gateway.model.CourseDto;
+import ru.mentor.gateway.api.AdminCourseControllerApi;
 import ru.mentor.services.RedirectAdminCourseService;
+
+import java.util.List;
 
 /**
  * Контроллер управления курсами для администратора.
  */
 @RestController
-@RequestMapping("/admin/course")
+//@RequestMapping("/admin/course")
 @RequiredArgsConstructor
-@Tag(name = "Admin Course Controller", description = "Управление курсами и их содержимым для админов.")
-class AdminCourseController {
+//@Tag(name = "Admin Course Controller", description = "Управление курсами и их содержимым для админов.")
+class AdminCourseController implements AdminCourseControllerApi {
 
     private final RedirectAdminCourseService redirectAdminCourseService;
 
-    /**
-     * Возвращает курс по его ID
-     *
-     * @param courseId
-     *         ID курса
-     *
-     * @return {@link CourseDto}
-     */
-    @Operation(
-            summary = "Получить курс по ID",
-            description = "Возвращает информацию о курсе по его идентификатору",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Информация о курсе",
-                            content = @Content(schema = @Schema(implementation = CourseDto.class))),
-                    @ApiResponse(responseCode = "401", description = "Не авторизован"),
-                    @ApiResponse(responseCode = "404", description = "Курс не найден")
-            }
-    )
-    @GetMapping("/{courseId}")
-    public ResponseEntity<CourseDto> getCourseById(@PathVariable Long courseId) {
-        return ResponseEntity.ok().body(redirectAdminCourseService.getCourseById(courseId));
-    }
+//    /**
+//     * Возвращает курс по его ID
+//     *
+//     * @param courseId
+//     *         ID курса
+//     *
+//     * @return {@link CourseDto}
+//     */
+//    @Operation(
+//            summary = "Получить курс по ID",
+//            description = "Возвращает информацию о курсе по его идентификатору",
+//            responses = {
+//                    @ApiResponse(responseCode = "200", description = "Информация о курсе",
+//                            content = @Content(schema = @Schema(implementation = CourseDto.class))),
+//                    @ApiResponse(responseCode = "401", description = "Не авторизован"),
+//                    @ApiResponse(responseCode = "404", description = "Курс не найден")
+//            }
+//    )
+//    @GetMapping("/{courseId}")
+//    public ResponseEntity<CourseDto> getCourseById(@PathVariable Long courseId) {
+//        return ResponseEntity.ok().body(redirectAdminCourseService.getCourseById(courseId));
+//    }
 
     /**
-     * Возвращает страницу курсов
-     *
-     * @param pageNumber
-     *         номер страницы
-     *
-     * @param pageSize
-     *         размер страницы
-     *
-     * @return возвращает объект {@link Page}, содержащий объекты {@link CourseDto}
+     * Реализация ручки GET /admin/course/{courseId}
      */
-    @Operation(
-            summary = "Получить все курсы",
-            description = "Возвращает список всех курсов, включая неактивные",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Список всех курсов",
-                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = CourseDto.class)))),
-                    @ApiResponse(responseCode = "401", description = "Не авторизован"),
-            }
-    )
-    @GetMapping("/all")
-    public ResponseEntity<Page<CourseDto>> getAllCourses(
-            @RequestParam int pageNumber,
-            @RequestParam int pageSize
-    ) {
-        return ResponseEntity.ok().body(redirectAdminCourseService.getAllCourses(pageNumber, pageSize));
+    @Override
+    public ResponseEntity<CourseDto> adminGetCourseById(Long courseId) {
+        CourseDto course = redirectAdminCourseService.getCourseById(courseId);
+        return ResponseEntity.ok(course);
+    }
+
+//    /**
+//     * Возвращает страницу курсов
+//     *
+//     * @param pageNumber
+//     *         номер страницы
+//     *
+//     * @param pageSize
+//     *         размер страницы
+//     *
+//     * @return возвращает объект {@link Page}, содержащий объекты {@link CourseDto}
+//     */
+//    @Operation(
+//            summary = "Получить все курсы",
+//            description = "Возвращает список всех курсов, включая неактивные",
+//            responses = {
+//                    @ApiResponse(responseCode = "200", description = "Список всех курсов",
+//                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = CourseDto.class)))),
+//                    @ApiResponse(responseCode = "401", description = "Не авторизован"),
+//            }
+//    )
+//    @GetMapping("/all")
+//    public ResponseEntity<Page<CourseDto>> getAllCourses(
+//            @RequestParam int pageNumber,
+//            @RequestParam int pageSize
+//    ) {
+//        return ResponseEntity.ok().body(redirectAdminCourseService.getAllCourses(pageNumber, pageSize));
+//    }
+
+    /**
+     * Реализация ручки GET /admin/course/all
+     */
+    @Override
+    public ResponseEntity<List<CourseDto>> adminGetAllCourses(Integer pageNumber, Integer pageSize) {
+        List<CourseDto> courses = redirectAdminCourseService.getAllCourses(pageNumber, pageSize);
+        return ResponseEntity.ok(courses);
     }
 
 }
