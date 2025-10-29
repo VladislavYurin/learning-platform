04f758bfe6bd28938a6ffca5560373c5e8f353c0
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2025-10-29 14:26:25 +0600

    LEPL-23: промежуточные изменения по таске. Переделал TagController.

--- api-gateway/src/main/java/ru/mentor/controller/TagController.java
+++ api-gateway/src/main/java/ru/mentor/controller/TagController.java
@@ -1,39 +1,28 @@
 package ru.mentor.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.mentor.dto.tag.CourseTagDto;
+import ru.mentor.gateway.api.TagControllerApi;
+import ru.mentor.gateway.model.CourseTagDto;
+import ru.mentor.mapper.CourseTagDtoMapper;
 import ru.mentor.services.RedirectTagService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/course-tag")
 @RequiredArgsConstructor
-@Tag(name = "Tag Controller", description = "Получение всех тегов курсов")
-public class TagController {
+public class TagController implements TagControllerApi {
 
     private final RedirectTagService redirectTagService;
+    private final CourseTagDtoMapper courseTagDtoMapper;
 
-    @Operation(
-            summary = "Получить все теги курсов",
-            description = "Возвращает список всех тегов курсов",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Список всех тегов курсов",
-                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = CourseTagDto.class)))),
-                    @ApiResponse(responseCode = "401", description = "Не авторизован"),
-            }
-    )
-    @GetMapping("/all")
+    /**
+     * Реализация ручки GET /course-tag/all
+     */
+    @Override
     public ResponseEntity<List<CourseTagDto>> getAllTags() {
-       return redirectTagService.getAllTags();
+        List<ru.mentor.dto.tag.CourseTagDto> listCommonCourseTagDto = redirectTagService.getAllTags().getBody();
+        List<CourseTagDto> listApiCourseTagDto = courseTagDtoMapper.toListApiDto(listCommonCourseTagDto);
+        return ResponseEntity.ok(listApiCourseTagDto);
     }
 }
